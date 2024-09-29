6bb95db171c5911d61b9308375955c4f6c5a2a53
Tejaswini M B <110235711+tejaswini0-0@users.noreply.github.com>
2024-09-29 11:16:45 +0530

    Edited firebase configuration

--- config/firebase.tsx
+++ config/firebase.tsx
@@ -8,14 +8,7 @@ import 'firebase/database';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDLF_zhyKu4cobqnMsCFIICrayer-1sQMo",
-  authDomain: "top-tyre.firebaseapp.com",
-  projectId: "top-tyre",
-  storageBucket: "top-tyre.appspot.com",
-  messagingSenderId: "669309193936",
-  appId: "1:669309193936:web:141ba586c0b8eb2a17d8d5",
-  measurementId: "G-VVFK119J07",
-  databaseURL: "https://top-tyre-default-rtdb.asia-southeast1.firebasedatabase.app",
+<your-firebase-config>
 };
 
 const app = initializeApp(firebaseConfig);
